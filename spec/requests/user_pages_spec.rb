d72f4e0c020330e3b122ead0b916adcbd619808f
Allan Mercado <mapes911@gmail.com>
2013-01-29 11:56:01 -0800

    finish exercises for Chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,9 +26,93 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      # setup the test to fill in some of the fields, but not all
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: ""
+      end
+
+      # make sure that a form that fails does not change our user count
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      # now check different scenarios
+      describe "after submission - no password confirm" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_content("Password confirmation can't be blank")}
+      end
+
+      describe "after submission - no password" do
+        before do
+          fill_in "Confirmation", with: "foobar"
+          fill_in "Password",     with: ""
+          click_button submit
+        end
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content("Password confirmation can't be blank")}
+        it { should have_content("Password can't be blank")}
+      end
+
+      describe "after submission - password too short" do
+        before do
+          fill_in "Confirmation", with: "foobar"
+          fill_in "Password",     with: "foo"
+          click_button submit
+        end
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content("Password confirmation can't be blank")}
+        it { should have_content("Password is too short (minimum is 6 characters)")}
+      end
+
+      describe "after submission - no name" do
+        before do
+          fill_in "Confirmation", with: "foobar"
+          fill_in "Name",     with: ""
+          click_button submit
+        end
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content("Password confirmation can't be blank")}
+        it { should have_content("Name can't be blank")}
+      end
+
+      describe "after submission - no email" do
+        before do
+          fill_in "Confirmation", with: "foobar"
+          fill_in "Email",     with: ""
+          click_button submit
+        end
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content("Password confirmation can't be blank")}
+        it { should have_content("Email can't be blank")}
+      end
+
+      describe "after submission - invalid email" do
+        before do
+          fill_in "Confirmation", with: "foobar"
+          fill_in "Email",     with: "myemail is awesome"
+          click_button submit
+        end
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content("Password confirmation can't be blank")}
+        it { should have_content("Email is invalid")}
+      end
+
     end
 
     describe "with valid information" do
@@ -41,6 +125,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
