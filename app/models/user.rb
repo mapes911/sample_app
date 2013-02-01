29cba5acc321fb15ac5022a80944e59e3215554d
Allan Mercado <mapes911@gmail.com>
2013-02-01 15:18:39 -0800

    Add user following and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   # callbacks
   before_save { self.email.downcase! }
@@ -33,6 +39,23 @@ class User < ActiveRecord::Base
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
   end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
+
 
   private
     def create_remember_token
